53ac576367088c6be01a430a75d46a942015dab3
ivan-26work <ivanworkers2026@gmail.com>
2026-03-28 10:04:37 +0000

    Create rappel.js

---
+++ js/rappel.js
@@ -0,0 +1,9 @@
+// Test simple : notification toutes les 2 minutes
+setInterval(() => {
+  if (Notification.permission === 'granted') {
+    showNotif('🔔 TEST MEGANE_LEARN', 'La notification fonctionne !');
+    console.log('Notification envoyée à', new Date().toLocaleTimeString());
+  }
+}, 120000); // 120000 ms = 2 minutes
+
+console.log('Test de rappel activé - Notification toutes les 2 minutes');
